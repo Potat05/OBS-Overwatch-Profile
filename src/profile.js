a36f4e48f38f80dda0e050833ae705bdddf76e1b
Potato <80370292+Potat05@users.noreply.github.com>
2021-12-19 15:38:38 -0600

    remove default profile

--- src/profile.js
+++ src/profile.js
@@ -1,64 +1,64 @@
 
-// let profile = {};
+let profile = {};
 
 let games = null;
 
 // Testing profile so we dont spam the api when changing stuff
 
-let profile = {
-    "competitiveStats": {
-        "awards": {
-            "cards": 36,
-            "medals": 386,
-            "medalsBronze": 146,
-            "medalsSilver": 130,
-            "medalsGold": 110
-        },
-        "games": {
-            "played": 162,
-            "won": 83
-        }
-    },
-    "endorsement": 2,
-    "endorsementIcon": "https://static.playoverwatch.com/svg/icons/endorsement-frames-3c9292c49d.svg#_2",
-    "gamesWon": 4579,
-    "icon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/daeddd96e58a2150afa6ffc3c5503ae7f96afc2e22899210d444f45dee508c6c.png",
-    "level": 41,
-    "levelIcon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/fa410247dd3f5b7bf2eb1a65583f3b0a3c8800bcd6b512ab1c1c4d9dd81675ae.png",
-    "name": "kerbybit#1483",
-    "prestige": 13,
-    "prestigeIcon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/1858704e180db3578839aefdb83b89054f380fbb3d4c46b3ee12d34ed8af8712.png",
-    "private": false,
-    "quickPlayStats": {
-        "awards": {
-            "cards": 1656,
-            "medals": 14082,
-            "medalsBronze": 4423,
-            "medalsSilver": 4916,
-            "medalsGold": 4743
-        },
-        "games": {
-            "played": 5309,
-            "won": 2706
-        }
-    },
-    "rating": 3042,
-    "ratingIcon": "https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/rank-DiamondTier.png",
-    "ratings": [
-        {
-            "level": 3136,
-            "role": "tank",
-            "roleIcon": "https://static.playoverwatch.com/img/pages/career/icon-tank-8a52daaf01.png",
-            "rankIcon": "https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/rank-DiamondTier.png"
-        },
-        {
-            "level": 2948,
-            "role": "damage",
-            "roleIcon": "https://static.playoverwatch.com/img/pages/career/icon-offense-6267addd52.png",
-            "rankIcon": "https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/rank-PlatinumTier.png"
-        }
-    ]
-}
+// let profile = {
+//     "competitiveStats": {
+//         "awards": {
+//             "cards": 36,
+//             "medals": 386,
+//             "medalsBronze": 146,
+//             "medalsSilver": 130,
+//             "medalsGold": 110
+//         },
+//         "games": {
+//             "played": 162,
+//             "won": 83
+//         }
+//     },
+//     "endorsement": 2,
+//     "endorsementIcon": "https://static.playoverwatch.com/svg/icons/endorsement-frames-3c9292c49d.svg#_2",
+//     "gamesWon": 4579,
+//     "icon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/daeddd96e58a2150afa6ffc3c5503ae7f96afc2e22899210d444f45dee508c6c.png",
+//     "level": 41,
+//     "levelIcon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/fa410247dd3f5b7bf2eb1a65583f3b0a3c8800bcd6b512ab1c1c4d9dd81675ae.png",
+//     "name": "kerbybit#1483",
+//     "prestige": 13,
+//     "prestigeIcon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/1858704e180db3578839aefdb83b89054f380fbb3d4c46b3ee12d34ed8af8712.png",
+//     "private": false,
+//     "quickPlayStats": {
+//         "awards": {
+//             "cards": 1656,
+//             "medals": 14082,
+//             "medalsBronze": 4423,
+//             "medalsSilver": 4916,
+//             "medalsGold": 4743
+//         },
+//         "games": {
+//             "played": 5309,
+//             "won": 2706
+//         }
+//     },
+//     "rating": 3042,
+//     "ratingIcon": "https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/rank-DiamondTier.png",
+//     "ratings": [
+//         {
+//             "level": 3136,
+//             "role": "tank",
+//             "roleIcon": "https://static.playoverwatch.com/img/pages/career/icon-tank-8a52daaf01.png",
+//             "rankIcon": "https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/rank-DiamondTier.png"
+//         },
+//         {
+//             "level": 2948,
+//             "role": "damage",
+//             "roleIcon": "https://static.playoverwatch.com/img/pages/career/icon-offense-6267addd52.png",
+//             "rankIcon": "https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/rank-PlatinumTier.png"
+//         }
+//     ]
+// }
 
 
 
