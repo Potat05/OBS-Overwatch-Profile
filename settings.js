87538fbde7c48b3b218cab5a413425826e02622f
Potato <80370292+Potat05@users.noreply.github.com>
2021-12-18 23:42:24 -0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+
+
+const settings = {
+    profile_name: 'kerbybit',
+    profile_id: '1483',
+    platform: 'pc',
+    region: 'us',
+
+    profile_update_interval_ms: 10 * 60 * 1000 // 10 minutes
+}
